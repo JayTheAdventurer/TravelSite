14f45cde4b62cc3704c86ff299aff6fb0409b415
Jay <jaytheadventurer1.gmail.com>
2021-03-11 11:47:32 -0500

    Made it to Section 5, CSS architecture. Many files have been added, such as a webpack system and css styling.

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,2 @@
+import '../styles/styles.css'
+//calls upon css file in directory
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const { loader } = require('mini-css-extract-plugin')
+const path = require('path') //initalize new const var path
+//css plugins that are used by css, and must be recognised by webpack
+const postCSSplugins = [ 
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
+    require('autoprefixer')
+]
+//Bundler export file//
+module.exports = {
+    entry: './app/assets/scripts/App.js', //webpack.config.js calls on app.js 
+    output: {
+        filename: 'bundled.js', //huge web file writes to bundled.js
+        path: path.resolve(__dirname, 'app') //use the directory 'app'
+    },
+    mode: 'development',
+    watch: true, //watches for file changes after compiling the code 'nrm run dev'
+   
+    //css options code
+    module:{
+        //sets when the webpack should bundle and use the css code
+        rules:[
+            {
+                test: /\.css$/i, //if the fille is a .css file
+                use: ['style-loader','css-loader', {loader: 'postcss-loader', options:{ postcssOptions: {plugins: postCSSplugins}} }  ]
+            }//use the style, css loader, and post css loader, with these plugins and our manual settings
+        ]
+    }
+}
